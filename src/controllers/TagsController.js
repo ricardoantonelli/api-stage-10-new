d4b46d12dfe9c628feecbee7d5b139ac647f388e
ricardo <ricardo_fabbro@hotmail.com>
2023-08-15 19:40:41 -0300

    Upload de imagens

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -4,7 +4,8 @@ class TagsController {
   async index(request, response) {
     const user_id = request.user.id
 
-    const tags = await knex('tags').where({ user_id })
+    const tags = await knex('tags')
+      .where({ user_id })
 
     return response.json(tags)
   }
